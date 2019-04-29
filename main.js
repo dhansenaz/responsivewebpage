d2a017442f91d7c0f969ee7affc2f57ef72d5bda
dhansenaz <dhansen@allthingsdot.com>
2019-04-29 08:56:01 -0700

    added on submit modal

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let modal = document.getElementById('myModal');
+let btn = document.getElementById('button');
+let span = document.getElementsByName('close')[0];
+
+
 $(function () {
     $('.menu-toggle, .fa-times').on('click', function () {
         $('nav').toggleClass('active');
@@ -10,3 +15,16 @@ $(function () {
     });  
 
 });
+
+btn.onclick = function() {
+    modal.style.display = 'block'
+}
+
+span.onclick = function() {
+    modal.style.display = 'none'
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none'
+    }
+}
